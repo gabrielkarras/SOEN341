db05e1013d18334fe706979d326c322dfbed578c
Minh Tran Nhat <minhtrannhat@tutanota.com>
2022-03-10 17:52:08 -0500

    #45 Began working on payment pages; created ShippingScreen, Payments page and CheckoutSteps file (#81)
    Authored-by: Victoria Castelli <vcastelli@bell.net>

---
+++ amazon/frontend/src/actions/ShippingActions.js
@@ -0,0 +1,15 @@
+//uncomment once cart is created?
+/*
+export const saveShippingAddress = (data) => (dispatch) => {
+
+    dispatch({
+        type: CART_SAVE_SHIPPING_ADDRESS,
+        payload: data,
+    })
+
+    localStorage.setItem('shippingAddrerss', JSON.stringify(data))
+
+    
+
+}
+*/
