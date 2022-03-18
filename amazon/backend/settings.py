9025cd6a87fb2f88f2d93d48b54b55f9ceb10ae0
gabrielkarras <gavrilkarras@hotmail.com>
2022-03-17 21:28:45 -0400

    Added localhost:8000 to cors whitelist

--- amazon/backend/settings.py
+++ amazon/backend/settings.py
@@ -160,6 +160,10 @@ REST_FRAMEWORK = {
 }
 
 # Port to interact with React
-CORS_ORIGIN_WHITELIST = ["http://localhost:3000", "http://127.0.0.1:8000" ]
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
 
 AUTH_USER_MODEL = "model_api.Client"
